a368447233b2471921ed686f95919d745510dd68
Victor Henrique <34209601+VictorHenrique13@users.noreply.github.com>
2017-12-16 10:40:57 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-Echo"eita carai"
+require_once("config.php");
+$sql =  new sql();
+
+$banco = $sql->select("SELECT * FROM DB_USUARIO ORDER BY idusu");
+$header = array();
+foreach ($banco[0] as $key => $value ) {
+	array_push($header, ucfirst($key));
+}
+echo implode("/", $header);
+
+$file = fopen("usuario.csv", "w+");
+fwrite($file, implode("/", $header));
 
 ?>
